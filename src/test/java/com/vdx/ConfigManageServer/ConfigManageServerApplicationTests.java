5cf8a8005b0308dfc6e718ee66d3c42115e28d21
Aresh Babu Anumolu <areshbabu@gmail.com>
2021-11-07 02:40:22 +0530

    Updated tests

--- src/test/java/com/vdx/ConfigManageServer/ConfigManageServerApplicationTests.java
+++ src/test/java/com/vdx/ConfigManageServer/ConfigManageServerApplicationTests.java
@@ -1,13 +1,24 @@
 package com.vdx.ConfigManageServer;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 @SpringBootTest
+//@RunWith(SpringRunner.class)
+@ExtendWith(SpringExtension.class)
+@WebAppConfiguration()
+//@ContextConfiguration(classes = {TestWebConfig.class, TestBackEndConfiguration.class})
+@TestInstance(Lifecycle.PER_CLASS)
 class ConfigManageServerApplicationTests {
-
+	
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 	}
 
 }
